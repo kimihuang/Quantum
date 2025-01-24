247f443c8ebc49a1b99bbb6ec54081f102245b0a
lion <schuangliang@126.com>
2025-01-24 10:43:41 +0800

    修改download脚本, 可以通过输入参数选择下载源码仓库

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,19 +1,49 @@
 #!/bin/bash
 
-# 下载所需的源码仓库
-#REPO_URLS=(
-#    "https://github.com/ARM-software/arm-trusted-firmware.git"
-#    "https://source.denx.de/u-boot/u-boot.git"
-#    "https://kernel.org/pub/linux/kernel/v5.x/linux-5.10.17.tar.xz"
-#    "https://buildroot.org/downloads/buildroot-2021.02.tar.gz"
-#    "https://github.com/RT-Thread/rt-thread.git"
-#)
+# 检查参数
+if [ "$#" -eq 0 ]; then
+    echo "Usage: $0 [all|kernel|tfa|uboot|buildroot|rt-thread]"
+    exit 1
+fi
 
-REPO_URLS=(
-    "https://source.denx.de/u-boot/u-boot.git"
-    "https://buildroot.org/downloads/buildroot-2021.02.tar.gz"
-    "https://github.com/RT-Thread/rt-thread.git"
-)
+# 根据参数设置要下载的仓库
+case "$1" in
+    all)
+        echo "Downloading all repositories..."
+        REPO_URLS=(
+            "https://github.com/u-boot/u-boot.git"
+            "https://buildroot.org/downloads/buildroot-2021.02.tar.gz"
+            "https://github.com/RT-Thread/rt-thread.git"
+            "https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"
+            "https://github.com/ARM-software/arm-trusted-firmware.git"
+        )
+        ;;
+    kernel)
+        echo "Downloading kernel repositories..."
+        REPO_URLS=("https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git")
+        ;;
+    tfa)
+        echo "Downloading arm-trusted-firmware repositories..."
+        REPO_URLS=("https://github.com/ARM-software/arm-trusted-firmware.git")
+        ;;
+    uboot)
+        echo "Downloading uboot repositories..."
+        REPO_URLS=("https://github.com/u-boot/u-boot.git")
+        ;;
+    buildroot)
+        echo "Downloading buildroot repositories..."
+        REPO_URLS=("https://buildroot.org/downloads/buildroot-2024.08.tar.xz")
+        ;;
+    rt-thread)
+        echo "Downloading rt-thread repositories..."
+        REPO_URLS=("https://github.com/RT-Thread/rt-thread.git")
+        ;;
+    *)
+        echo "Invalid option: $1"
+        echo "Usage: $0 [all|uboot|buildroot|rt-thread]"
+        exit 1
+        ;;
+esac
 
 # 下载源码
 for REPO in "${REPO_URLS[@]}"; do

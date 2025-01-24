4a049fe9231aeacb1b482103c90667b3ecb3622c
lion <schuangliang@126.com>
2025-01-24 13:43:10 +0800

    提交构建kernel的脚本

--- scripts/build/build_kernel.sh
+++ scripts/build/build_kernel.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-# 编译内核的脚本
+# 设置 Kernel 构建相关路径
+ROOT_DIR=$(pwd)
+ROOT_OUT_DIR="$ROOT_DIR/out"
+KERNEL_OUT_DIR="$ROOT_OUT_DIR/kernel_out"
+KERNEL_SRC_DIR="$ROOT_DIR/src/kernel"
 
-# 设置工作目录
-KERNEL_SRC_DIR="../src/kernel"
-BUILD_DIR="../../build"
-OUTPUT_DIR="../../out"
 
-# 创建输出目录
-mkdir -p $OUTPUT_DIR
+# 判断 kernel out 目录是否存在，不存在则创建
+if [ ! -d "$KERNEL_OUT_DIR" ]; then
+    echo "kernel out directory not found, creating $KERNEL_OUT_DIR"
+    mkdir -p "$KERNEL_OUT_DIR"
+fi
 
-# 进入内核源代码目录
+# 判断 kernel 源码目录是否存在，不存在则输出调试信息后退出
+if [ ! -d "$KERNEL_SRC_DIR" ] && [ ! -L "$KERNEL_SRC_DIR" ]; then
+    # 创建软链接到 linux 源码目录
+    echo "kernel source directory is not a symlink, creating soft link to $KERNEL_SRC_DIR"
+    ln -s /home/lighthouse/sourcecode/linux-master $KERNEL_SRC_DIR
+fi
+
+# 进入内核源码目录
 cd $KERNEL_SRC_DIR
 
+# 清理编译产物
+#make ARCH=arm64 mrproper
+make O=$KERNEL_OUT_DIR ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- mrproper
+
+# 配置内核以适配 QEMU
+make O=$KERNEL_OUT_DIR ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- defconfig
+
 # 执行编译命令
-make -j$(nproc) O=$BUILD_DIR
+make O=$KERNEL_OUT_DIR ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc)
 
-# 将编译产物复制到输出目录
-cp $BUILD_DIR/* $OUTPUT_DIR/
 
-echo "内核编译完成，产物已输出到 $OUTPUT_DIR"
+echo "内核编译完成，产物已输出到 $KERNEL_OUT_DIR"
